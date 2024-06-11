d1e18ee0ca8b3f4502da546bab2d5394b08e3466
Delique Allen <allen_delique@columbusstate.edu>
2024-06-11 16:14:21 -0400

    fix modules-1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { useDataToggle } = require("./modules/useDataToggle");
 const { useState } = require("./modules/useState");
 const { useRouter } = require("./modules/useRouter");
 
-exports.modules = {
+module.exports = {
     useDataToggle,
     useState,
     useRouter
